7bf91331d644298c4de4ffb7a5e52d0975735671
Your Name <you@example.com>
2023-03-20 00:56:17 +0500

    commit task

---
+++ Задача/Program.cs
@@ -0,0 +1,28 @@
+﻿//Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
+string[] a=new string[] {"1234","1567","-2","computer scince"};
+string[] b=new string[a.Length];
+Array.Copy(a,b,a.Length);
+Console.WriteLine("Массив a: ");
+PrintArray(a);
+Console.WriteLine();
+Console.WriteLine("Массив b(копия a): ");
+PrintArray(b);
+Console.WriteLine();
+System.Console.WriteLine("Измененный массив a: ");
+ChangeArray (a);
+
+void ChangeArray(string[] a)
+{   int i=0;
+    foreach (var value in a )
+    {
+     if (value.Length <= 3)
+        System.Console.Write($"{a[i]} ");
+        i++;
+    }
+}
+
+void PrintArray(string[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i]} ");
+}
